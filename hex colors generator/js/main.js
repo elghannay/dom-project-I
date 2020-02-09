77f5a263361b9c8392228c214d6a971bfa629927
Elghannay Mohamed <mohamedelghannay@gmail.com>
2020-02-09 11:56:57 +0100

    finished the hex color generator

--- hex colors generator/js/main.js
+++ hex colors generator/js/main.js
@@ -1,8 +1,32 @@
 'use strict';
+const span = document.querySelector('span');
 const btn = document.getElementById('btn');
-let colors = ['#24324F', '#457ABF', '#1CA68A', '#FFD769', '#FF3E4C'];
-btn.addEventListener('click', function() {
-  let random = Math.floor(Math.random() * colors.length);
-  console.log(random);
-  document.body.style.backgroundColor = colors[random];
-});
+
+let colors = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'
+];
+let random;
+btn.addEventListener('click', generateHex);
+function generateHex() {
+  // console.log(random);
+  let hex = '#';
+  for (let index = 0; index < 6; index++) {
+    random = Math.floor(Math.random() * colors.length);
+    hex = hex + colors[random];
+  }
+  span.textContent = hex;
+  document.body.style.backgroundColor = hex;
+}
