e0906fc33eea0ec0489634d0f671f438e84be206
elghannay <mohamedelghannay@gmail.com>
2020-05-08 05:05:46 +0000

    finishing the application -bugs++

--- Timer/dom centric approach.js
+++ Timer/dom centric approach.js
@@ -1,15 +1,30 @@
 const startButton = document.querySelector('#startButton');
 const pauseButton = document.querySelector('#pauseButton');
 const duration = document.querySelector('#duration');
+const circle = document.querySelector('circle');
+const radius = circle.getAttribute('r');
+const perimeter = radius * Math.PI * 2;
+let totalDuration = 0;
 
 const timer = new Timer(startButton, pauseButton, duration, {
-    onStart() {
-        console.log('start');
+    onStart(total) {
+        circle.setAttribute('stroke-dasharray', perimeter);
+        totalDuration = total;
     },
-    onTick() {
-        console.log('the timer is ticking');
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset', (perimeter * timeRemaining / totalDuration - perimeter));
     },
     onfinish() {
-        console.log('the timer has finished');
+        circle.setAttribute('stroke-dashoffset', 0);
     }
 });
+
+
+/* bugs to fix for the moment and features to add
+when pausing the animation start from zero
+when clicking for the second time on the start button
+the pause does not work
+add a logging system to track how many hours have you worked each day
+add a min timer instead of seconds
+*/
+
--- Timer/timer.js
+++ Timer/timer.js
@@ -12,15 +12,15 @@ class Timer {
         this.pauseButton.addEventListener('click', this.pause);
     }
     start = () => {
-        // the error that i was making is if(this.callback) which has no sense.
+        // the error that i was making > if(this.callback) which has no sense.
         // so i should instead access the instance of the properties that i have set when
         // the callback exists.
         if (this.onStart)
-            this.onStart();
+            this.onStart(this.timeRemaining);
         this.tick();
         this.intervalId = setInterval(() => {
             this.tick();
-        }, 1000);
+        }, 20);
     }
     tick = () => {
         if (this.timeRemaining <= 0) {
@@ -30,15 +30,15 @@ class Timer {
         }
         else {
             if (this.onTick)
-                this.onTick();
-            this.timeRemaining = this.timeRemaining - 1;
+                this.onTick(this.timeRemaining);
+            this.timeRemaining = this.timeRemaining - 0.02;
         }
     }
     get timeRemaining() {
         return this.timeRemaining = parseFloat(this.duration.value);
     }
     set timeRemaining(time) {
-        return this.duration.value = time;
+        return this.duration.value = time.toFixed(2);
     }
     pause = () => {
         clearInterval(this.intervalId);
