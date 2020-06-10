5712fb4f6541db04bc285449b159ebcc2aac3476
elghannay <mohamedelghannay@gmail.com>
2020-06-10 02:19:42 +0100

    minor changes to the code

--- Timer/dom centric approach.js
+++ Timer/dom centric approach.js
@@ -5,11 +5,13 @@ const circle = document.querySelector('circle');
 const radius = circle.getAttribute('r');
 const perimeter = radius * Math.PI * 2;
 let totalDuration = 0;
+// we need totalDuration for calculating dashoffset 
 
 const timer = new Timer(startButton, pauseButton, duration, {
     onStart(total) {
         circle.setAttribute('stroke-dasharray', perimeter);
         totalDuration = total;
+        // stroke-dasharray(10) make the 10 pixels long dashes and 10 pixel long gaps
     },
     onTick(timeRemaining) {
         circle.setAttribute('stroke-dashoffset', (perimeter * timeRemaining / totalDuration - perimeter));
--- Timer/timer.js
+++ Timer/timer.js
@@ -4,6 +4,9 @@ class Timer {
         this.pauseButton = pauseButton;
         this.duration = duration;
         if (callbacks) {
+            // the callbacks are totally optional.
+            // that's why everyTime we do check if the methods
+            // does exists.
             this.onStart = callbacks.onStart;
             this.onTick = callbacks.onTick;
             this.onfinish = callbacks.onfinish;
@@ -11,6 +14,10 @@ class Timer {
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
+    // start:function(){} using the custom properties feature of js the arrow 
+    // function will actually will be defined on the constructor thus the VALID line
+    // code that is just above the function definition will be right on the constructor.
+    // so the this keyword will always refer to the instance of the class.
     start = () => {
         // the error that i was making > if(this.callback) which has no sense.
         // so i should instead access the instance of the properties that i have set when
@@ -18,6 +25,8 @@ class Timer {
         if (this.onStart)
             this.onStart(this.timeRemaining);
         this.tick();
+        // instead of declaring a variable with const we need to be able 
+        // to access it from anywhere on the class.
         this.intervalId = setInterval(() => {
             this.tick();
         }, 20);
@@ -31,9 +40,14 @@ class Timer {
         else {
             if (this.onTick)
                 this.onTick(this.timeRemaining);
+            // below we combine the setter and getter 
             this.timeRemaining = this.timeRemaining - 0.02;
+            // whatever the value on the right (getter) will be 
+            // provided as argument of the setter function.
         }
     }
+    // with setters and getters we don't need the parentheses
+    // as they will be treated as variables.
     get timeRemaining() {
         return this.timeRemaining = parseFloat(this.duration.value);
     }
