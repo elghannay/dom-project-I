ceef6aa10297c1eb5a8afb051a07d23c4a627c35
elghannay <mohamedelghannay@gmail.com>
2020-06-11 17:48:35 +0100

    intial data of films

--- movie API/index.js
+++ movie API/index.js
@@ -14,17 +14,81 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 }
 
-let div = document.createElement('div');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label for="input"><b>search for a movie</b></label>
+    <input id="input" class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+    `;
+
+const input = document.querySelector('.input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    dropdown.classList.add('is-active');
+    resultsWrapper.innerHTML = '';
+
     for (const movie of movies) {
-        div.innerHTML += `
-        <img src ="${movie.Poster}">
-        <h1>${movie.Title}</h1> `
+
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item');
+
+        option.innerHTML = `
+        <img src ="${imgSrc}">
+            ${movie.Title} `;
+        resultsWrapper.appendChild(option);
+
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        })
     }
 }
-const target = document.querySelector('#target');
-target.appendChild(div);
-const input = document.querySelector('input');
+document.addEventListener('click', event => {
+    if (!root.contains(event.target))
+        dropdown.classList.remove('is-active');
+})
+
+onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: apiKey,
+            i: movie.imdbID
+        }
+    })
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+}
+
+const movieTemplate = movieDetail => {
+    return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `;
+};
+
 input.addEventListener('input', debounce(onInput, 500));
--- movie API/utils.js
+++ movie API/utils.js
@@ -9,9 +9,9 @@
 //         , 500);
 // }
 
-/* now we have a function that can be used 
-with any kind of dom element not only with inputs. */
-
+/* now we have a function that can be used to debounce
+ any kind of dom element not only with inputs. */
+// the debounce function return a function that accepts arguments
 const debounce = (func, delay = 1000) => {
     let timeoutId;
     return (...args) => {
@@ -22,3 +22,4 @@ const debounce = (func, delay = 1000) => {
             , delay);
     }
 }
+
