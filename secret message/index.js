36130a8658348063b285abca12acaa518888ca26
elghannay <mohamedelghannay@gmail.com>
2020-06-14 22:07:25 +0100

    completed the secret message app

--- secret message/index.js
+++ secret message/index.js
@@ -1,8 +1,24 @@
-document.querySelector('.form').addEventListener('submit', (event) => {
+const linkForm = document.querySelector('.link-form');
+const messageForm = document.querySelector('.message-form');
+let messageInput = document.querySelector('.message-input');
+let messageShow = document.querySelector('.message-show');
+let linkInput = document.querySelector('.link-input');
+
+const { hash } = window.location;
+
+const decoded = atob(hash.replace('#', ''));
+if (decoded) {
+  messageShow.classList.remove('hide');
+  messageForm.classList.add('hide');
+  document.querySelector('.message-show h1').innerHTML = decoded;
+}
+
+document.querySelector('.message-form').addEventListener('submit', (event) => {
   event.preventDefault();
-  let message = document.querySelector('.message-input').value;
-  let link = document.querySelector('.link-input').value;
-  console.log(message);
-  let encoded = btoa(message);
-  link = encoded;
+  let encoded = btoa(messageInput.value);
+  linkInput.value = `${window.location}#${encoded}`;
+  linkInput.select();
+
+  linkForm.classList.remove('hide');
+  messageForm.classList.add('hide');
 });
