d651ab2248e8329bc4f54c10c6d382a3d7b2008e
elghannay <mohamedelghannay@gmail.com>
2020-06-15 17:53:56 +0100

    first approach to read stats

--- node-ls/index.js
+++ node-ls/index.js
@@ -1,7 +1,27 @@
+#!/usr/bin/env node
+// npm link
 const fs = require('fs');
+
 fs.readdir(process.cwd(), (err, files) => {
-  // if err the error object will be returned
-  // otherwise the err = null;
   if (err) console.log(err);
-  console.log(files);
+
+  const arrayStats = Array(files.length).fill(null);
+  for (const file of files) {
+    const index = files.indexOf(file);
+
+    fs.lstat(file, (err, stats) => {
+      if (err) console.log(err);
+      arrayStats[index] = stats;
+
+      const ready = arrayStats.every((stats) => {
+        return stats;
+      });
+
+      if (ready) {
+        arrayStats.forEach((stats, index) => {
+          console.log(files[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
