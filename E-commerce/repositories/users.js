42b62a85f23ced43a7c8b8aece3ba3d2aa80ed82
elghannay <mohamedelghannay@gmail.com>
2020-06-17 18:16:46 +0100

    repo done

--- E-commerce/repositories/users.js
+++ E-commerce/repositories/users.js
@@ -1,33 +1,95 @@
 const fs = require('fs');
-const { json } = require('express');
+const crypto = require('crypto');
 
-class usersRepository {
+class UsersRepository {
   constructor(filename) {
-    //   we used the sync version since we will be only creating
-    //   once instance of the users Class.
-    //   constructor in js are not allowed to be async in nature.
-    if (!filename)
-      throw new Error('a file must be created before having a userRepository');
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
+    }
 
     this.filename = filename;
-
     try {
-      fs.accessSync(filename);
-    } catch (e) {
+      fs.accessSync(this.filename);
+    } catch (err) {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
   async getAll() {
     return JSON.parse(
       await fs.promises.readFile(this.filename, {
-        encoding: 'utf-8',
+        encoding: 'utf8',
       })
     );
   }
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attrs);
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async getOne(id) {
+    return await this.getAll().find((element) => element.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filtered = records.filter((element) => element.id !== id);
+    await this.writeAll(filtered);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((element) => element.id === id);
+    if (!record) throw new Error(`the user with the id ${id} is not found`);
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) return record;
+    }
+  }
 }
-const test = async () => {
-  const repo = new usersRepository('users.json');
-  const data = await repo.getAll();
-  console.log(data);
-};
-test();
+
+module.exports = new UsersRepository('users.json');
+
+// class testing logic
+
+// const test = async () => {
+//   const repo = new UsersRepository('users.json');
+//   // await repo.create({ email: 'test@test.com', password: 'password' });
+//   // const users = await repo.getAll();
+//   // await repo.delete('4682738e');
+
+//   // const user = await repo.getOneBy({
+//   //   email: 'test@test.csom',
+//   // });
+//   // console.log(user);
+// };
+
+// test();
