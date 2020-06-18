f725b82734d5fd8b51df356234a89d218ac5aa6a
elghannay <mohamedelghannay@gmail.com>
2020-06-18 15:48:35 +0100

    adding a template

--- E-commerce/views/admin/signin.js
+++ E-commerce/views/admin/signin.js
@@ -1,9 +1,13 @@
+const template = require('./../template');
+
 module.exports = () => {
-  return `
+  return template({
+    content: `
 <form method="post">
     <input name="email" placeholder="email">
     <input name="password" placeholder="password">
     <button>sign in</button>
 </form>
-  `;
+  `,
+  });
 };
--- E-commerce/views/admin/signup.js
+++ E-commerce/views/admin/signup.js
@@ -1,10 +1,14 @@
+const template = require('./../template');
+
 module.exports = ({ req }) => {
-  return `
+  return template({
+    content: `
 <form method="post">
     <input name="email" placeholder="email">
     <input name="password" placeholder="password">
     <input name="confirmPassword" placeholder="confirm password">
     <button>sign up</button>
 </form>
-  `;
+  `,
+  });
 };
