3dc3ee51fffd19a8f4a7806fd81598976eb503f6
elghannay <mohamedelghannay@gmail.com>
2020-06-19 07:27:01 +0100

    done styling

--- E-commerce/views/admin/signup.js
+++ E-commerce/views/admin/signup.js
@@ -4,16 +4,36 @@ const { getError } = require('../helpers');
 module.exports = ({ errors }) => {
   return template({
     content: `
-<form method="post">
-    <input name="email" placeholder="email">
-    ${getError(errors, 'email')}
-    <input name="password" placeholder="password">
-    ${getError(errors, 'password')}
-    <input name="confirmPassword" placeholder="confirm password">
-    ${getError(errors, 'confirmPassword')}
-    <button>sign up</button>
-</form>
-  `,
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-quarter">
+            <form method="POST">
+              <h1 class="title">Sign Up</h1>
+              <div class="field">
+                <label class="label">Email</label>
+                <input required class="input" placeholder="Email" name="email" />
+                <p class="help is-danger">${getError(errors, 'email')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password</label>
+                <input required class="input" placeholder="Password" name="password" type="password" />
+                <p class="help is-danger">${getError(errors, 'password')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password Confirmation</label>
+                <input required class="input" placeholder="Password Confirmation" name="confirmPassword" type="password" />
+                <p class="help is-danger">${getError(
+                  errors,
+                  'confirmPassword'
+                )}</p>
+              </div>
+              <button class="button is-primary">Submit</button>
+            </form>
+            <a href="/signin">Have an account? Sign In</a>
+          </div>
+        </div>
+      </div>
+    `,
   });
 };
 
