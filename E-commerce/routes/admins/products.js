8ad2a4ed847db83831ccf95fa469a6fe0bbe261c
elghannay <mohamedelghannay@gmail.com>
2020-06-25 01:23:55 +0100

    fixing products routes

--- E-commerce/routes/admins/products.js
+++ E-commerce/routes/admins/products.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productsRepo = require('../../repositories/products');
 const productTemplate = require('../../views/admin/products/new');
-router.get('./admins/products', (req, res) => {});
-router.get('./admins/products/new', (req, res) => {
+router.get('/admins/products', (req, res) => {});
+router.get('/admins/products/new', (req, res) => {
   res.send(productTemplate({}));
 });
 
